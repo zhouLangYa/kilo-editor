96270b81ce084e0a830060dfbdf7d0ec5ad44c60
ruituan.zhou <zhouruituan@163.com>
2024-04-20 13:17:13 +0800

    step 10: disable ctrl-s and ctrl-q
    Signed-off-by: ruituan.zhou <zhouruituan@163.com>

--- kilo.c
+++ kilo.c
@@ -19,6 +19,7 @@ void enableRawMode()
 	atexit(disableRawMode);
 
 	raw = orig_termios;
+	raw.c_iflag &= ~(IXON);
 	raw.c_lflag &= ~(ECHO | ICANON | ISIG);
 
 	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
