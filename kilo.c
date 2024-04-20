23920f4822991782f1a82b0d92812c774d629b62
ruituan.zhou <zhouruituan@163.com>
2024-04-20 13:12:59 +0800

    step 9: turn off ctrl-c and ctrl-z signals
    Signed-off-by: ruituan.zhou <zhouruituan@163.com>

--- kilo.c
+++ kilo.c
@@ -19,7 +19,7 @@ void enableRawMode()
 	atexit(disableRawMode);
 
 	raw = orig_termios;
-	raw.c_lflag &= ~(ECHO | ICANON);
+	raw.c_lflag &= ~(ECHO | ICANON | ISIG);
 
 	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
