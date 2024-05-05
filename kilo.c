3908ea1158b63a82677898f01e6af243ff132bcd
zhouLangYa <2770156115@qq.com>
2024-05-05 18:30:09 +0800

    step 18: call die() when fail
    Signed-off-by: zhouLangYa <2770156115@qq.com>

--- kilo.c
+++ kilo.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
-
+#include <errno.h>
 struct termios orig_termios;
 
 void die(const char *s)
@@ -14,14 +14,17 @@ void die(const char *s)
 
 void disableRawMode()
 {
-	tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+		die("tcsetattr");
 }
 
 void enableRawMode()
 {
 	struct termios raw;
 
-	tcgetattr(STDIN_FILENO, &orig_termios);
+	if (tcgetattr(STDIN_FILENO, &orig_termios) == -1)
+		die("tcgetattr");
+
 	atexit(disableRawMode);
 
 	raw = orig_termios;
@@ -32,7 +35,8 @@ void enableRawMode()
 	raw.c_cc[VMIN] = 0;
 	raw.c_cc[VTIME] = 1;
 
-	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1)
+		die("tcsetattr");
 }
 
 int main()
@@ -41,7 +45,10 @@ int main()
 
 	while (1) {
 		char c = '\0';
-		read(STDIN_FILENO, &c, 1);
+
+		if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN)
+			die("read");
+
 		if (iscntrl(c)) {
 			printf("%d\r\n", c);
 		} else {
