46fc24fd75c1f02ecf69a0e810c12fac6148df4f
zhouLangYa <2770156115@qq.com>
2024-05-05 18:10:02 +0800

    step 15: turn off more micellaneous flags
    Signed-off-by: zhouLangYa <2770156115@qq.com>

--- kilo.c
+++ kilo.c
@@ -19,8 +19,9 @@ void enableRawMode()
 	atexit(disableRawMode);
 
 	raw = orig_termios;
-	raw.c_iflag &= ~(IXON | ICRNL);
+	raw.c_iflag &= ~(IXON | ICRNL| BRKINT | INPCK | ISTRIP);
 	raw.c_oflag &= ~(OPOST);
+	raw.c_cflag |= (CS8);
 	raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN);
 
 	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
